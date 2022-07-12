ffab051fb33b3b4f9426ac8d527409e5b5568768
KYPremco <KYPremco@gmail.com>
2022-07-12 11:49:54 +0200

    Added default vue layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,13 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => {
+        const page = resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'));
+        page.then((module) => {
+            module.default.layout = module.default.layout || DefaultLayout;
+        });
+        return page;
+    },
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
